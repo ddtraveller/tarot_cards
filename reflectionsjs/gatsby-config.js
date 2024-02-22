66dbc3fe16e84255b41bfac8bda7fd8171f53ba6
Ubuntu <ubuntu@ip-172-31-18-59.us-west-2.compute.internal>
2024-02-22 18:55:54 +0000

    code example

---
+++ reflectionsjs/gatsby-config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  
+  siteMetadata: {
+    title: `reflectionsjs`,
+    pathPrefix: `/reflections`,
+    siteUrl: `https://tarot.clearlightdoor.com/reflections`,
+  },
+  plugins: [],
+}
+module.exports = {
+ 
+}
